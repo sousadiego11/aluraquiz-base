3a6a494fffe334d462dac8417cc65d0233db73db
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2021-01-26 20:18:06 -0300

    commit segundo dia de imersão

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 import db from '../db.json'
+import Head from 'next/head'
+import React from 'react'
+import {useRouter} from 'next/router'
+
 import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
@@ -23,20 +27,55 @@ export const QuizContainer = styled.div`
     padding: 15px;
   }
 `
+export const QuizBotao = styled.button`
+  width: 100%;
+  height: 40px;
+
+  background-color: ${({ theme }) => theme.colors.secondary};
+
+  margin-top: 30px;
+
+  border-radius: 7px;
+  border: 0;
+`
+export const QuizInput = styled.input`
+  width: 100%;
+  height: 40px;
+
+  border-radius: 7px;
+  border: 0;
+`
 
 export default function Home() {
+  
+  const router = useRouter() 
+  const [name, setName] = React.useState('')
+
   return (
   <QuizBackground backgroundImage={db.bg}>
+    <Head>
+    <title>Alura Quiz DnD</title>
+    </Head>
     <QuizContainer>
     <QuizLogo/>
-      
       <Widget>
         <Widget.Header>
-          <h1>Teste seus conhecimentos em JavaScript!</h1>
+          <h1>Dungeons and Dragons</h1>
         </Widget.Header>
           
         <Widget.Content>
-          <p>Quão bem você anda estudando?</p>
+          <p>Teste seus conhecimentos nesse famoso RPG de mesa!</p>
+          <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}} onSubmit={function(infoEvento){
+              infoEvento.preventDefault()
+              router.push(`/quiz?name=${name}`)
+            }} >
+            <QuizInput onChange={function(infoEvento){
+              setName(infoEvento.target.value)
+            }} placeholder="Digita ai o seu nome! :D"/>
+            <QuizBotao type="submit" disabled={name.length === 0} >
+              JOGAR {name}
+            </QuizBotao>
+          </form>
         </Widget.Content>
       </Widget>
       
---
+++ pages/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pudim(){
+ return(
+     <h1>Teste</h1>
+ )   
+}
+
+export default Pudim
